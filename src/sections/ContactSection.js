58678faa0d7ecc14a017a818c248135d75198ef1
abhikam395 <--add>
2021-03-31 21:15:31 +0530

    added responsiveness

--- src/sections/ContactSection.js
+++ src/sections/ContactSection.js
@@ -1,22 +1,25 @@
 import React,{Component} from 'react';
 import './contact.scss';
+import {HiLocationMarker, HiPhone, HiMail} from 'react-icons/hi'
 
 export default class ContactSection extends Component {
     render(){
         return (
             <section id="contact" 
-                className="contactsection contactsection--size contactsection--theme">
-                <div className="center contactsection__content">
+                className="contactsection 
+                    contactsection--size 
+                    contactsection--theme">
+                <div className="center 
+                    contactsection__content 
+                    contactsection__content--size">
                     <form className="form form--size ">
-                        <div className="form__row form__mt">
-                            <div className="form__box">
-                                <label className="form__label">FIRST NAME *</label>
-                                <input type="text" className="form__input"/>
-                            </div>
-                            <div className="form__box">
-                                <label className="form__label">LAST NAME *</label>
-                                <input type="text" className="form__input"/>
-                            </div>
+                        <div className="form__box">
+                            <label className="form__label">FIRST NAME *</label>
+                            <input type="text" className="form__input"/>
+                        </div>
+                        <div className="form__box">
+                            <label className="form__label">LAST NAME *</label>
+                            <input type="text" className="form__input"/>
                         </div>
                         <div className="form__box form__mt">
                             <label className="form__label">EMAIL *</label>
@@ -33,8 +36,23 @@ export default class ContactSection extends Component {
                         <button className="form__button form__button--size">Send Message</button>
                     </form>
                     <hr className="contactsection__line"></hr>
-                    <h3 className="contactsection__info">Contact Information</h3>
-                    <div className="contactsection__contact"></div>
+                    <div className="contactsection__contact">
+                        <h3 className="contactsection__info">Contact Information</h3>
+                        <div className="contactsection__row">
+                            <HiLocationMarker />
+                            <h5 className="contactsection__label">Deal Raipur road 
+                                Dehradun, Uttarakhand, India
+                            </h5>
+                        </div>
+                        <div className="contactsection__row">
+                            <HiPhone />
+                            <h5 className="contactsection__label">9997260969</h5>
+                        </div>
+                        <div className="contactsection__row">
+                            <HiMail />
+                            <h5 className="contactsection__label">abhikam395@gmail.com</h5>
+                        </div>
+                    </div>
                 </div>
             </section>
         )
